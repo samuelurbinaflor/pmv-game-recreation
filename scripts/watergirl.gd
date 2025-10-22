d7e49223675a0e6e5fb6c68e137fe3fae0697a9f
Samuel <samuelurbinaflor@gmail.com>
2025-10-22 18:18:05 +0200

    updt

---
+++ scripts/watergirl.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("w_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("w_left", "w_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+
+ # Mover y detectar colisión
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		var layer = collision.get_collider().get_instance_id()
+		if layer != 34829501831:
+			print(layer)
+	
