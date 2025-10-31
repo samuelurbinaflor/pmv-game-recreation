a7b1f2501b64cbb34e08bf2347cb0b33bb5072af
Samuel Urbina Flor <samuelurbinaflor@gmail.com>
2025-10-31 12:49:45 +0100

    .

--- scripts/button.gd
+++ scripts/button.gd
@@ -14,7 +14,8 @@ func _ready():
 	start_position = position
 	end_position = position + Vector2(0, 3)
 	sprite_2d.modulate = color
-	target_object.set_color(color)
+	if target_object:
+		target_object.set_color(color)
 
 
 func _physics_process(delta: float) -> void:
@@ -27,14 +28,17 @@ func _physics_process(delta: float) -> void:
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	bodies_on_area += 1
 	_active = true
-	target_object.set_button_active(true)
+	if target_object:
+		target_object.set_button_active(true)
 	
 	
 func _on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	bodies_on_area -= 1
-	target_object.set_button_active(false)
-	
+
 	if bodies_on_area <= 0:
 		_active = false
+		
+	if target_object:
+		target_object.set_button_active(false)
 	
 	
