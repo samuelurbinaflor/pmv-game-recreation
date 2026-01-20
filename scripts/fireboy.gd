bd7b4ba60e6b0f1e9a748bd957b3ca16321221d2
Samuel Urbina Flor <samuelurbinaflor@gmail.com>
2026-01-20 13:29:46 +0100

    red gem

--- scripts/fireboy.gd
+++ scripts/fireboy.gd
@@ -1,5 +1,5 @@
 extends Player
-
+var gem_counter = 0
 func _ready() -> void:
 	if len(multiplayer.get_peers()) > 0 :
 		input_scheme = "w"
@@ -10,3 +10,14 @@ func _ready() -> void:
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("red_gem"):
 		area.queue_free()
+		gem_counter += 1
+		report_gem_collected.rpc()
+
+
+@rpc("any_peer")
+func report_gem_collected():
+	if not multiplayer.is_server():
+		return
+
+	var level := get_tree().get_first_node_in_group("level")
+	level.on_red_gem_collected()
--- scripts/level.gd
+++ scripts/level.gd
@@ -5,16 +5,45 @@ extends Node2D
 @onready var level_completed: Control = $level_completed
 @onready var level_interface: Control = $level_interface
 
-@export var spawners: Array[MultiplayerSpawner]
+var red_gems_total := 0
+var red_gems_collected := 0
 
+var blue_gems = 0
+var fb_scene
+var wg_scene
+
+@export var spawners: Array[MultiplayerSpawner]
+func _ready() -> void:
+	## condición multiplayer
+	if len(multiplayer.get_peers()) > 0:
+		# Preconfigure game.
+		if not multiplayer.is_server():
+			Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
+		else:
+			start_game()
+	else:
+		## instantiate local players
+		fb_scene = $Spawners/fb_multiplayer.network_player
+		wg_scene = $Spawners/wg_mutiplayer.network_player
+		get_node("Spawners/wg_spawn").add_child(wg_scene.instantiate())
+		get_node("Spawners/fb_spawn").add_child(fb_scene.instantiate())
+		
+	red_gems_total =  get_tree().get_nodes_in_group("red_gem").size()
+	#blue_gems =  get_tree().get_nodes_in_group("blue_gem").size()
+
+	
 func _process(_delta: float) -> void:
 	if blue_door.is_fully_open and red_door.is_fully_open:
 		print("end level")
 		end_level()
-		
-	#if blue_door.is_fully_open and red_door.is_fully_open:
-		#set_pause(true)
-		#level_completed.visible = true
+	
+	
+func on_red_gem_collected():
+	red_gems_collected += 1
+
+	if red_gems_collected == red_gems_total:
+		print("todas las gemas rojas recogidas")
+
 
 func end_level():
 	set_pause.rpc(true)
@@ -33,21 +62,7 @@ func show_level_completed():
 	level_interface.visible = false
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	## condición multiplayer
-	if len(multiplayer.get_peers()) > 0:
-		# Preconfigure game.
-		if not multiplayer.is_server():
-			Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
-		else:
-			start_game()
-	else:
-		## instantiate local players
-		var fb_scene = $Spawners/fb_multiplayer.network_player
-		var wg_scene = $Spawners/wg_mutiplayer.network_player
-		get_node("Spawners/wg_spawn").add_child(wg_scene.instantiate())
-		get_node("Spawners/fb_spawn").add_child(fb_scene.instantiate())
-		
+
 
 func start_game():
 	# All peers are ready to receive RPCs in this scene.
